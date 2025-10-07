4dc6d9812d347ba296a22a9a97e59ae404587d6c
Nguyen Quoc Chi <chinguyen123852@gmail.com>
2025-10-07 09:48:20 +0700

    rental service, room image

---
+++ src/main/java/com/example/rental/repository/RentalServiceRepository.java
@@ -0,0 +1,7 @@
+package com.example.rental.repository;
+
+import com.example.rental.entity.RentalServiceItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RentalServiceRepository extends JpaRepository<RentalServiceItem, Long> {
+}
